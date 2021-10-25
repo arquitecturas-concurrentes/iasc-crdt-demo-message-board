9fa9aa6b205963dc161e7fbea7aec51734e02b89
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-10-24 21:16:26 -0300

    initial commit

---
+++ build_image.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t iasc-valtio-yjs-minimal-example:latest .
---
+++ run_image.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --name iasc-valtio-yjs -d -p 3000:3000 iasc-valtio-yjs-minimal-example:latest
